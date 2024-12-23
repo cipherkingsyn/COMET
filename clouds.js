793162df720ef22c58310adf8ba9717f33c47b72
cipherkingsyn <160332792+cipherkingsyn@users.noreply.github.com>
2024-12-23 07:31:42 -0500

    Create clouds.js

---
+++ clouds.js
@@ -0,0 +1,17 @@
+<script src="three.r134.min.js"></script>
+<script src="vanta.clouds.min.js"></script>
+<script>
+VANTA.CLOUDS({
+  el: "#clouds",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  skyColor: 0x1d1d1e,
+  sunColor: 0xffffff,
+  sunGlareColor: 0x1e1e1e,
+  sunlightColor: 0x0,
+  speed: 1.90
+})
+</script>
